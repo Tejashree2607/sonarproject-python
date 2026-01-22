cfbe2546d5783812715e5d0fa6196729534fffcb
Tejashree2607 <tejashreegurav01@gmail.com>
2026-01-22 23:30:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+
+def add_numbers(a, b):
+  return a + b
+
+def substract_numbers(a, b):
+  return a - b
+
+def main():
+  x = 10
+  y = 5
+  print("Addition:", add_numbers(x, y))
+  print("Substraction:", substract_numbers(x, y))
+
+if __name__ == "__main__:
+  main()
